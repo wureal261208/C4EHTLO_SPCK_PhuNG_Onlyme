1f7720a425855962a25d3e8cd3c6029cc79bb697
Kai Aetherweigh <136946134+ImKaiMinhLam@users.noreply.github.com>
2026-03-02 21:59:01 +0700

    Update script(acc).js

--- web/main/script(acc).js
+++ web/main/script(acc).js
@@ -101,6 +101,7 @@ function renderBooks(books) {
                 <div class="card-meta">
                     <span><i class='bx bx-book-open'></i> ${book.pages} pages</span>
                     <span><i class='bx bx-calendar'></i> ${new Date().getFullYear()}</span>
+                    <span><i class='bx bx-eye'></i> ${book.views || 12} views</span>
                 </div>
                 <div class="card-footer">
                     <span class="category-tag">${book.genre}</span>
